43cde9ba53d159f7899680eafab1734f82e57fac
PhuccNguyen <nguyenhoangphuc315@gmail.com>
2024-03-23 11:26:16 +0700

    New font-end

--- assets/js/script.js
+++ assets/js/script.js
@@ -63,3 +63,45 @@
     })
   })
 
+document.addEventListener("DOMContentLoaded", function () {
+    // Function to handle publishing all contributions
+    document.querySelector(".publish-all-btn").addEventListener("click", function () {
+        publishAllContributions();
+    });
+
+    // Function to handle rejecting all contributions
+    document.querySelector(".reject-all-btn").addEventListener("click", function () {
+        rejectAllContributions();
+    });
+
+    function publishAllContributions() {
+        console.log("All contributions published.");
+    }
+
+    // Sample function to reject all contributions (replace with your logic)
+    function rejectAllContributions() {
+        console.log("All contributions rejected.");
+    }
+});
+//toggleDetail for detail contributions
+ function toggleDetail(button) {
+    var detailDiv = button.nextElementSibling;
+    detailDiv.classList.toggle('show');
+}
+
+
+// Function to update the date display
+function updateDate() {
+    // Create a new Date object
+    var currentDate = new Date();
+    var day = currentDate.getDate();
+    var month = currentDate.getMonth() + 1; 
+    var year = currentDate.getFullYear();
+    var formattedDate = year + "-" + (month < 10 ? "0" + month : month) + "-" + (day < 10 ? "0" + day : day);
+    // Update the date display in the HTML element with id="currentDate"
+    document.getElementById("currentDate").textContent = formattedDate;
+}
+updateDate();
+setInterval(updateDate, 86400000);
+
+
