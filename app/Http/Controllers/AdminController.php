29f744abb9744494a0027a8a6c8afaeb37bd4354
PhuccNguyen <nguyenhoangphuc315@gmail.com>
2024-04-19 22:12:35 +0700

    feat(admin): edit delete student

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Mail\EmailCreatePassword;
 use App\Models\Contribution;
 use App\Models\Faculty;
 use App\Models\Magazine;
@@ -13,10 +14,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use Illuminate\View\View;
+use Illuminate\Validation\Rules;
 
 
 class AdminController extends Controller
@@ -35,6 +38,49 @@ class AdminController extends Controller
         ]);
     }
 
+    public function student_edit(Request $request)
+    {
+        $user = User::where('id', $request->id)->first();
+        $faculty = Faculty::where('status', 0)->get();
+        return view('/administrators/student-edit', [
+            'user' => $user,
+            'faculty' => $faculty
+        ]);
+    }
+
+    public function student_edit_save(Request $request): RedirectResponse
+    {
+        $user = User::find($request->id);
+
+        if (!$user) {
+            abort(404);
+        }
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->phone = $request->phone;
+        $user->roles_id = $request->role;
+        $user->years_of_university = $request->year;
+        $user->faculty_id = $request->faculty;
+
+        if ($request->filled('password')) {
+            $user->password = bcrypt($request->password);
+        }
+
+        $user->save();
+        return Redirect::route('admin.student');
+    }
+
+    public function student_delete(Request $request)
+    {
+        User::where('id', $request->id)->update(["status"=>1]);
+        $user = User::where(['roles_id' => 4, 'status' => 0])->get();
+        return view('/administrators/student', [
+            'user' => $user
+        ]);
+    }
+
+
     public function academic_year_manage()
     {
         $magazine = Magazine::where('status', 0)->get();
@@ -43,10 +89,10 @@ class AdminController extends Controller
         ]);
     }
 
+
     public function marketing_coordinator_manage()
     {
         $user = User::where(['roles_id' => 2, 'status' => 0])->get();
-        //lấy danh sách giáo viên year thuộc khoa của giáo viên
         return view('/administrators/marketing-coordinator', [
             'user' => $user
         ]);
---
+++ resources/views/administrators/student-edit.blade.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Responsive Dashboard using HTML, CSS, and JavaScript</title>
+    <link
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+            rel="stylesheet"
+    />
+    <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+    />
+    <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link rel="stylesheet" href="{{ asset('/css/style-dashboard.css') }}">
+</head>
+<body>
+<div id="error-message" style="display: none;" data-error="{{ session('error') }}"></div>
+<div class="container">
+    <aside>
+        <div class="top">
+            <div class="logo">
+                <img src="https://guxdryphbnffhexbtcvn.supabase.co/storage/v1/object/public/magazine-contribution-bucket/icon-logo.webp" />
+                <h2>Greenwich</h2>
+            </div>
+            <div class="close" id="close-btn">
+                <span class="material-icons">close</span>
+            </div>
+        </div>
+        <div class="sidebar">
+            <a href="{{ route('admin.home') }}">
+                <span class="material-icons">grid_view</span>
+                <h3>Dashboard</h3>
+            </a>
+            <a href="{{ route('admin.student') }}" class="active">
+                <span class="material-icons">person</span>
+                <h3>Student</h3>
+            </a>
+            <a href="{{ route('admin.mc') }}">
+                <span class="material-icons">receipt_long</span>
+                <h3>Marketing Coordinator</h3>
+            </a>
+            <a href="{{ route('admin.academic') }}">
+                <span class="material-icons">insights</span>
+                <h3>Academic year</h3>
+            </a>
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+
+                <a href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+
+                    <span class="material-icons">logout</span>
+                    <h3>Log Out</h3>
+                </a>
+            </form>
+        </div>
+    </aside>
+
+    <section class="student-form form-i-main">
+        <div class="student-form-name form-main">
+            <h2 class="form-name">Edit Student</h2>
+        </div>
+        <form class="form-information-wrapper" action="{{ route('user.edit-save') }}" method="post" enctype="multipart/form-data">
+            @csrf
+            @method('patch')
+            <input type="hidden" name="id" value="{{ $user->id }}">
+            <div class="form-information-wrap">
+                <label for="name-student">Name</label>
+                <input id="name-student" type="text" placeholder="Name" name="name" value="{{ $user->name }}">
+            </div>
+            <div class="form-information-wrap">
+                <label for="email-student">Email</label>
+                <input id="email-student" type="text" placeholder="Email" name="email" value="{{ $user->email }}">
+            </div>
+            <div class="form-information-wrap">
+                <label for="phone-student">Phone</label>
+                <input id="phone-student" type="text" placeholder="Phone" name="phone" value="{{ $user->phone }}">
+            </div>
+            <input type="hidden" name="role" value="4">
+            <div class="form-information-wrap">
+                <label >Year of university</label>
+                <select name="year">
+                    <option value="1" {{ $user->years_of_university == 1 ? 'selected' : '' }}>1</option>
+                    <option value="2" {{ $user->years_of_university == 2 ? 'selected' : '' }}>2</option>
+                    <option value="3" {{ $user->years_of_university == 3 ? 'selected' : '' }}>3</option>
+                    <option value="4" {{ $user->years_of_university == 4 ? 'selected' : '' }}>4</option>
+                </select>
+            </div>
+            <div class="form-information-wrap">
+                <label >Faculty ID</label>
+                <select name="faculty">
+                    @foreach($faculty as $info)
+                        <option value="{{ $info->id }}" @if($info->id === $user->faculty_id) selected @endif>{{ $info->faculty }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="student-form-btn form-main-btn">
+                <button class="cancel-btn" type="button">Cancel</button>
+                <button class="save-student-btn">Save</button>
+            </div>
+        </form>
+    </section>
+
+
+
+</div>
+<script src="{{ asset('/js/jquery-3.2.1.min.js') }}"></script>
+<script src="{{ asset('/js/popper.min.js') }}"></script>
+<script src="{{ asset('/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('/js/owl.carousel.min.js') }}"></script>
+<script src="{{ asset('/js/owl.carousel.js') }}"></script>
+<script src="{{ asset('/js/script.js') }}"></script>
+<script src="{{ asset('/js/dropdown.js') }}"></script>
+<script src="{{ asset('/js/cancel.js') }}"></script>
+<script src="{{ asset('/js/error.js') }}"></script>
+</body>
+</html>
